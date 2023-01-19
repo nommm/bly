79bbfb30d8ce2ccd03d8cbdd943b0e863ea0f082
쪼썽민 <77090591+nommm@users.noreply.github.com>
2023-01-19 22:28:42 +0700

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -3,9 +3,6 @@ echo "You must to install NodeJs"
 pause
 start "Chrome" chrome --new-window https://nodejs.org/en/
 pause
-npm update
-npm i git
-npm update
 npm install -g --unsafe-perm node-red
 start "Chrome" chrome --new-window http://127.0.0.1:1880/
 start "Chrome" chrome --new-page http://www.hivemq.com/demos/websocket-client/
